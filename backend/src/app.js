ed29bcdea5f03b775cbfdaa3d91f5c5d06e7fa3a
goodmiravite-oss <goodmiravite@gmail.com>
2026-03-17 15:52:44 +0800

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,8 +6,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ROUTES
+const authRoutes = require('./modules/auth/auth.routes');
+const userRoutes = require('./modules/users/user.routes');
+const jobRoutes = require('./modules/jobs/job.routes');
+const bidRoutes = require('./modules/bids/bid.routes');
+const walletRoutes = require('./modules/wallet/wallet.routes');
+const escrowRoutes = require('./modules/escrow/escrow.routes');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/jobs', jobRoutes);
+app.use('/api/bids', bidRoutes);
+app.use('/api/wallet', walletRoutes);
+app.use('/api/escrow', escrowRoutes);
+
+// TEST ROUTE
 app.get('/', (req, res) => {
-  res.send('Jack of All Trades API is running 🚀');
+  res.send('Jack of All Trades API is LIVE 🚀');
 });
 
 module.exports = app;
